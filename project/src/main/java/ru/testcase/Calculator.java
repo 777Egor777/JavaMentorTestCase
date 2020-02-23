0308920844a32e55bb990ac8110d6c8b54acdeb0
Egor <yegeraskin13@gmail.com>
2020-02-23 21:05:41 +0400

    make frame of class Calculator

--- project/src/main/java/ru/testcase/Calculator.java
+++ project/src/main/java/ru/testcase/Calculator.java
@@ -1,7 +1,31 @@
 package ru.testcase;
 
+/**
+ * Class implements simple
+ * calculator, that could
+ * perform 4 base math operations:
+ * summation(+), subtraction(-),
+ * multiplication(*) and division(/)
+ * with two integer arguments
+ * @author Egor Geraskin
+ * @version 1.0, 14.01.2020
+ */
 public class Calculator {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {}
+
+    int summation(int firstNumber, int secondNumber) {
+        return 0;
+    }
+
+    int subtraction(int firstNumber, int secondNumber) {
+        return 0;
+    }
+
+    int multiplication(int firstNumber, int secondNumber) {
+        return 0;
+    }
+
+    int division(int firstNumber, int secondNumber) {
+        return 0;
     }
 }
