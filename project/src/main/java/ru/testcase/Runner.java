988d62f55e36d47bae47ee8971d4dffa6200b723
Egor <yegeraskin13@gmail.com>
2020-02-24 17:03:33 +0400

    Implemented methods of the class Runner

--- project/src/main/java/ru/testcase/Runner.java
+++ project/src/main/java/ru/testcase/Runner.java
@@ -23,11 +23,55 @@ public class Runner {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         DataInput dataInput = new DataInput(reader);
         Calculator calculator = new Calculator();
-        NumeralTransformer transformer = new NumeralTransformer();
 
+        int result = new Runner().resultOfOperation(calculator,
+                                                    dataInput.getFirstArgument(),
+                                                    dataInput.getSecondArgument(),
+                                                    dataInput.getOperation());
+        new Runner().outputResult(result, dataInput.isRomeNumeral());
     }
 
-    public int resultOfOperation(Calculator calculator, int firstNumber, int secondNumber, char operation) {
-        return 0;
+    /**
+     * Make operation with object
+     * calculator and numbers
+     * firstNumber, secondNumber
+     * @param calculator
+     * @param firstNumber
+     * @param secondNumber
+     * @param operation
+     * @return result of the operation
+     */
+    public int resultOfOperation(Calculator calculator, int firstNumber, int secondNumber, char operation) throws IOException {
+        int result = 0;
+        switch (operation) {
+            case '+': result = calculator.summation(firstNumber, secondNumber);
+                      break;
+            case '-': result = calculator.subtraction(firstNumber, secondNumber);
+                      break;
+            case '*': result = calculator.multiplication(firstNumber, secondNumber);
+                      break;
+            case '/': result = calculator.division(firstNumber, secondNumber);
+                      break;
+        }
+
+        return result;
+    }
+
+    /**
+     * Output the result in
+     * correct format
+     * @param result - result as Arabic numeral
+     * @param isRomeNumeral false - we should output this form
+     *                      true - we must transfer number
+     *                             to Rome numeral and then
+     *                             output it
+     */
+    public void outputResult(int result, boolean isRomeNumeral) {
+        if (!isRomeNumeral) {
+            System.out.println(result);
+        } else {
+            NumeralTransformer transformer = new NumeralTransformer();
+            System.out.println(transformer.changeArabicToRome(result));
+        }
     }
 }
